15adbce1157e5e551f001cd2a5b4ff9a82a93dee
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-04 01:11:16 +0100

    Compile

--- pictocodemaindialog.cpp
+++ pictocodemaindialog.cpp
@@ -1,23 +1,3 @@
-//---------------------------------------------------------------------------
-/*
-PicToCode, tool to convert a picture to C++ code
-Copyright (C) 2010-2015 Richel Bilderbeek
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.If not, see <http://www.gnu.org/licenses/>.
-*/
-//---------------------------------------------------------------------------
-//From http://www.richelbilderbeek.nl/ToolPicToCode.htm
-//---------------------------------------------------------------------------
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
 #pragma GCC diagnostic ignored "-Wunused-local-typedefs"
@@ -33,7 +13,6 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #include <QImage>
 
 #include "pictocodemenudialog.h"
-#include "testtimer.h"
 #pragma GCC diagnostic pop
 
 ribi::p2c::MainDialog::MainDialog()
@@ -66,8 +45,6 @@ void ribi::p2c::MainDialog::Test() noexcept
     if (is_tested) return;
     is_tested = true;
   }
-  const TestTimer test_timer(__func__,__FILE__,1.0);
-
   MainDialog d;
   d.SetInputImage(QImage(":/p2c/images/R.png"));
   d.SetGraphicsLibrary(GraphicsLibrary::nds);
